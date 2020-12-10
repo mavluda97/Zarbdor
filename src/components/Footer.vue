6ea9cc3ce20c665718210dcea14132157fe80964
mavluda97 <37047992+mavluda97@users.noreply.github.com>
2020-12-10 15:35:35 +0500

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <v-footer height="auto" class="toolb" color="#19D3CC">
     <v-layout justify-center row wrap>
       <v-flex color="indigo" dark py-3 text-xs-center white--text xs12>
-        <div style="display: flex; justify-content: space-between">
+        <div class="ftr">
           <img src="../assets/footer-logo.png" height="32" width="143" />
           <div>
             &copy;2020 —
@@ -10,6 +10,13 @@
               href="http://it-solutions.uz"
               style="text-decoration: none; color: white"
               ><strong>IT-Solutions</strong></a
+            >
+          </div>
+           <div>
+             <a
+              href="http://inha.uz"
+              style="text-decoration: none; color: white"
+              ><strong>Inha University in Tashkent</strong></a
             >
           </div>
         </div>
@@ -39,5 +46,15 @@ export default {
 .ftr {
   display: flex;
   justify-content: space-between;
+}
+@media (max-width:600px) {
+.ftr{
+  flex-direction: column;
+  justify-content: center;
+  row-gap: 10px;
+}  
+.ftr img{
+  margin: 0 auto;
+}
 }
 </style>
