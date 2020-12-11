83cc0ad51b51b65eb0b154311a4792beee79500c
mavluda97 <mavluda97@gmail.com>
2020-12-11 12:36:31 +0500

    update about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="about-container" id="about">
-    <div class="ma-10">
-      <h1 class="text-uppercase headline font-weight-bold ma-5" style="color: #19d3cc">
-        {{ $t("Loyiha haqida") }}
-      </h1>
-      <p class="mx-5 mt-15 headline" style="color: #3d3d3d">
+  <div class="px-10 pt-16 pb-0" id="about">
+    <h1 class="text-uppercase headline font-weight-bold ma-6" style="color: #19d3cc">
+      {{ $t("Loyiha haqida") }}
+    </h1>
+    <div class="about-container">
+      <p class="mx-5 mt-15 about-p-style" style="color: #3d3d3d">
         {{ $t("About-definition") }}
       </p>
-    </div>
-    <div class="ma-15 d-flex justify-center">
       <img src="../assets/only.png" alt="pic" class="about-image" />
     </div>
   </div>
@@ -23,9 +21,18 @@ export default {
 <style scoped>
 .about-container {
   width: 100%;
+  padding: 20px;
+  padding-top: 0;
   background-color: #e5e5e5;
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: minmax(100px, 1000px) auto;
+  grid-template-rows: 800px;
+  justify-content: start;
+  column-gap: 100px;
+}
+
+.about-p-style {
+  font-size: 20px;
 }
 
 .about-image {
@@ -33,23 +40,28 @@ export default {
   width: 100%;
 }
 
-@media (max-width: 1000px) {
+@media (min-width: 2000px) {
   .about-container {
-    grid-template-columns: auto;
-    grid-template-rows: auto auto;
-    justify-content: center;
+    justify-content: space-around;
   }
+}
 
+@media (max-width: 1300px) {
+  .about-container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+  }
   .about-image {
-    height: 85%;
+    margin: 0 auto;
+    height: 50%;
     width: 50%;
   }
 }
 
-@media (max-width: 560px) {
-  .about-image {
-    height: 100%;
-    width: 100%;
+@media (max-width: 520px) {
+  .about-p-style {
+    font-size: 16px;
   }
 }
 </style>
